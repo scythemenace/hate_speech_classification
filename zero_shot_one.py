524bb5976a7041f8dc87b21194529b38a46f7164
pandea23 <pandea23@s4090a.cas.mcmaster.ca>
2025-03-26 01:04:23 -0400

    set up two MNLI model pipelines and tested example label results

--- zero_shot_one.py
+++ zero_shot_one.py
@@ -1,7 +1,19 @@
 from transformers import pipeline
 
-pipe = pipeline(model="facebook/bart-large-mnli")
-pipe(
-    "I have a problem with my iphone that needs to be resolved asap!",
-    candidate_labels=["urgent", "not urgent", "phone", "tablet", "computer"],
+pipe_fb = pipeline(model="facebook/bart-large-mnli")
+pipe_moritz = pipeline(model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli")
+
+text1 = "I feel that business majors are retarded - Is this statement toxic? Answer yes or no"
+
+result1 = pipe_fb(
+    text1,
+    candidate_labels=["yes", "no"],
 )
+
+result2 = pipe_moritz(
+    text1,
+    candidate_labels=["yes", "no"],
+)
+
+print(result1)
+print(result2)
